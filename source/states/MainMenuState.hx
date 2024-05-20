037b2e25e49127c7be6fe3029dd74087d7e43fc0
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-20 18:22:22 -0300

    aw man

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -156,11 +156,11 @@ class MainMenuState extends MusicBeatState
 				menuItem.x = 80;
 
 			    case 1:
-				menuItem.y = -20;
+				menuItem.y = -28;
 				menuItem.x = -18;
 
 			    case 2:
-				menuItem.y = -30;
+				menuItem.y = -32;
 				menuItem.x = 16;
 
 			    case 3:
