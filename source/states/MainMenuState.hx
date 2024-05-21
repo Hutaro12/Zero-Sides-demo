9c9c1170dc0b2a99b608e7bb5add5ba115aff8da
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-20 22:17:47 -0300

    a

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -172,18 +172,18 @@ class MainMenuState extends MusicBeatState
 
 
 			
-            /*if (firstStart)
-				FlxTween.tween(menuItem, {x: -50}, 1 + (i * 0.25), {
-					ease: FlxEase.expoInOut,
-					onComplete: function(flxTween:FlxTween)
-					{
-					finishedFunnyMove = true;
-					changeItem();
-				}
-			});
-			else
-			menuItem.x= 50;
-		}*/
+            //if (firstStart)
+				//FlxTween.tween(menuItem, {x: -50}, 1 + (i * 0.25), {
+					//ease: FlxEase.expoInOut,
+					//onComplete: function(flxTween:FlxTween)
+					//{
+					//finishedFunnyMove = true;
+					//changeItem();
+				//}
+		   // });
+			//else
+			//menuItem.x= 50;
+		}
         firstStart = false;
 
 		//FlxG.camera.follow(camFollow, null, 0);
