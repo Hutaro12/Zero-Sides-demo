451042ec0a457563b74e46be38e729bb5a139f11
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-23 12:16:53 -0300

    Update MainMenuState.hxqq

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -144,7 +144,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.scale.y = 0.7;
 			menuItem.x = 50;
 			menuItem.scrollFactor.set(0, yScroll);
-			FlxTween.tween(menuItem, {x: menuItem.width / 4 + (i * 60) - 75}, 1.3, {ease: FlxEase.sineInOut});
+			//FlxTween.tween(menuItem, {x: menuItem.width / 4 + (i * 60) - 75}, 1.3, {ease: FlxEase.sineInOut});
 			menuItems.add(menuItem);
 			var scr:Float = (optionSelect.length - 4) * 0.135;
 			if(optionSelect.length < 6) scr = 0;
