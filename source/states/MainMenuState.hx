43eb5fee8ab9b265067d88f0552a0195d1fbf043
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-23 01:18:10 -0300

    a

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -148,7 +148,7 @@ class MainMenuState extends MusicBeatState
 			if(optionSelect.length < 6) scr = 0;
 			menuItem.antialiasing = ClientPrefs.data.antialiasing;
 			menuItem.updateHitbox();
-            if (firstStart)
+            //if (firstStart)
 				//FlxTween.tween(menuItem, {x: 50}, 1 + (i * 0.25), {
 					//ease: FlxEase.expoInOut,
 					//onComplete: function(flxTween:FlxTween)
