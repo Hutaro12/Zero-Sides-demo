dabc024054ec827089efd9cac28c43ea169d34e7
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-22 22:35:35 -0300

    Update..... Lol

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -147,11 +147,8 @@ class MainMenuState extends MusicBeatState
 			var scr:Float = (optionSelect.length - 4) * 0.135;
 			if(optionSelect.length < 6) scr = 0;
 			menuItem.antialiasing = ClientPrefs.data.antialiasing;
-			menuItem.updateHitbox();	
-			
-            switch (firstStart)
-			 {
-		         case 1:
+			menuItem.updateHitbox();		
+            if (firstStart)
 				FlxTween.tween(menuItem, {x: 50}, 1 + (i * 0.25), {
 					ease: FlxEase.expoInOut,
 					onComplete: function(flxTween:FlxTween)
@@ -160,8 +157,11 @@ class MainMenuState extends MusicBeatState
 					changeItem();
 				}
 			});
-	                //else
-			//menuItem.x= 50;
+			
+	        switch (i)
+	               {
+		           case 0:
+			   menuItem.x= 50;
 		}
         firstStart = false;
 
