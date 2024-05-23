dd3d97cbba234381e0ca65dfb711a6be822c13d4
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-23 12:41:50 -0300

    Update MainMenuState.hxa

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -133,22 +133,23 @@ class MainMenuState extends MusicBeatState
 
 		for (i in 0...optionSelect.length)
 		{
-			var offset:Float = 108 - (Math.max(optionSelect.length, 4) - 4) * 80;
-			var menuItem:FlxSprite = new FlxSprite(FlxG.width * -1.5, (i * 140)  + offset);
+                        var offset:Float = 108 - (Math.max(optionSelect.length, 4) - 4) * 80;
+			var menuItem:FlxSprite = new FlxSprite(0, (i * 140) + offset);
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionSelect[i]);
 			menuItem.animation.addByPrefix('idle', optionSelect[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionSelect[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
+			menuItem.screenCenter(X);
+			menuItem.x += 290;
 			menuItem.scale.x = 0.7;
 			menuItem.scale.y = 0.7;
-			menuItem.x = 50;
-			menuItem.scrollFactor.set(0, yScroll);
+			menuItem.scrollFactor.set();
 			//FlxTween.tween(menuItem, {x: menuItem.width / 4 + (i * 60) - 75}, 1.3, {ease: FlxEase.sineInOut});
 			menuItems.add(menuItem);
 			var scr:Float = (optionSelect.length - 4) * 0.135;
-			if(optionSelect.length < 6) scr = 0;
-			menuItem.antialiasing = ClientPrefs.data.antialiasing;
+			if(optionSelect.length < 6) scr = 0;	
+			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
 			menuItem.updateHitbox();     
 		}
 
@@ -212,12 +213,6 @@ class MainMenuState extends MusicBeatState
    		#end
 
 		super.create();
-	}
-
-	override function closeSubState() {
-		changeItem(0, false);
-		persistentUpdate = true;
-		super.closeSubState();
 	}
 
 	var selectedSomething:Bool = false;
