47aac4119d2cc182f2623eed06fc17e99b697dd6
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-23 22:39:25 -0300

    TEST 3

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -153,7 +153,7 @@ class MainMenuState extends MusicBeatState
 			switch (i)
 			{
 			    case 0:
-				FlxTween.tween(menuItem, {x:100}, 2.4, {ease: FlxEase.expoInOut} onComplete: function(flxTween:FlxTween)
+				FlxTween.tween(menuItem, {x:100}, 2.4, {ease: FlxEase.expoInOut, onComplete: function(flxTween:FlxTween)
 				menuItem.y = 2;
 
 			    case 1:
