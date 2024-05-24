4d17becbbcae1ed826f3e91ffe81ca16c9b27b16
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-24 20:15:14 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -177,7 +177,7 @@ class MainMenuState extends MusicBeatState
 			        changeItem();
 			               }	
 			        });
-				menuItem.y = 28.4;
+				menuItem.y = 28;
 
 			    case 3:
 				FlxTween.tween(menuItem, {x:104}, 1.4, {ease: FlxEase.expoInOut, onComplete: function(flxTween:FlxTween) 
