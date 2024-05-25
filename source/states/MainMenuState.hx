7bd9f67a1f0e73c7b44f81f318e09227f8cb3b8e
HutaroZ <153041319+Hutaro12@users.noreply.github.com>
2024-05-25 20:11:11 -0300

    TEST 5 YAAAYYY

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -107,11 +107,11 @@ class MainMenuState extends MusicBeatState
 		mainSide.updateHitbox();
 		mainSide.screenCenter();
 		mainSide.antialiasing = ClientPrefs.data.antialiasing;
-		mainSide.x = 950;
+		mainSide.x = 1500;
 		mainSide.y = -90;
 		add(mainSide);
 
-		FlxTween.tween(mainSide, {x: 1000}, 2.2, {ease: FlxEase.quartInOut});
+		FlxTween.tween(mainSide, {x: 700}, 2.2, {ease: FlxEase.quartInOut});
 		
 
 		menuItems = new FlxTypedGroup<FlxSprite>();
@@ -166,7 +166,7 @@ class MainMenuState extends MusicBeatState
 			        changeItem();
 			               }	
 			        });
-				menuItem.y = 12;
+				menuItem.y = 10;
 
 			    case 3:
 				FlxTween.tween(menuItem, {x:104}, 2.2, {ease: FlxEase.expoInOut, onComplete: function(flxTween:FlxTween) 
